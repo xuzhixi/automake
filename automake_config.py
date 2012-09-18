35868332c9140ce123e32e10828e65b1a9d082fc
xuzhixi <932834199@163.com>
2012-09-18 13:02:22 +0800

    automake 0.2.1.0
    1. 编译的中间文件扩展名可以在 g_mid_ext 和 g_debug_mid_ext 进行配置
    2. make install所执行的执行的命令，可以在g_install_commands列表进行配置

--- automake_config.py
+++ automake_config.py
@@ -5,6 +5,11 @@
 
 
 #user global set
+g_program_type = 1			# 1 表示一般程序, 2 表示动态库, 3 表示静态库
+g_handle_subdir = True	# 是否递归对子目录进行编译链接
+g_except_dir_list = []	# 不进行编译链接处理的目录, 仅当g_handle_subdir设置为True时才有效
+g_except_file_list = [] # 不进行编译链接处理的文件
+
 g_bin_name = "test"	# 生成程序名或动态库名或静态库名
 g_debug_bin_name = "test_debug"	# 执行make debug时生成的程序名
 
@@ -16,8 +21,5 @@ g_libs_opt = ""	# 包含的动态库选项
 
 g_ar_opt = "" # 编译静态库时, ar命令的选项
 
-g_program_type = 1			# 1 表示一般程序, 2 表示动态库, 3 表示静态库
-g_except_file_list = [] # 不进行编译链接处理的文件
-g_handle_subdir = True	# 是否递归对子目录进行编译链接
-g_except_dir_list = []	# 不进行编译链接处理的目录, 仅当g_handle_subdir设置为True时才有效
-
+# install执行命令列表
+g_install_commands = ["echo 'install command not set'"]
